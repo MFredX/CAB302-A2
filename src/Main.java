12938a9c5eb83ee5d15b23c586562f34e3d3044b
ManavTiwana <48900401+ManavTiwana@users.noreply.github.com>
2019-05-24 17:11:38 +1000

    so far

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
-public class Main {
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
+
+public class Main extends JApplet{
+    /**
+     * The public class main is extends JApplet which is a feature of JAVA swing public class designed for developers
+     * and the Applet basically is a small application that runs within the scope of a dedicated widget engine.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello Manav!");
+        JFrame window= new JFrame("Enjoy Painting!!!");
+        //System.out.println("Hello Wodjbfgjngjrld!");
+        //SimplePaintPanel content = new SimplePaintPanel();
+        // window.setContentPane(content);
+
+        window.setLocation(250,150);
+        window.setSize(650,500);
+        window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        window.setVisible(true);
     }
+/*
+    public void controlPanel(){setContentPane(new SimplePaintPanel());
+    }
+
+    */
+
 }
